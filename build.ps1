86d2d36d61fbc56cd2c4ce4685758994e09f120f
Scighost <scighost@outlook.com>
2023-05-20 16:23:31 +0800

    update build script

--- build.ps1
+++ build.ps1
@@ -7,16 +7,16 @@
 $ErrorActionPreference = "Stop";
 
 if ($Dev) {
-    dotnet publish Starward -c Release -r "win10-$Architecture" -o "build/Starward/app_$Version" -p:Platform=$Architecture -p:DefineConstants=DEV -p:PublishReadyToRun=true -p:PublishTrimmed=true -p:TrimMode=partial -p:Version=$Version;
+    dotnet publish Starward -c Release -r "win10-$Architecture" -o "build/Starward/app-$Version" -p:Platform=$Architecture -p:DefineConstants=DEV -p:PublishReadyToRun=true -p:PublishTrimmed=true -p:TrimMode=partial -p:Version=$Version;
 }
 else {
-    dotnet publish Starward -c Release -r "win10-$Architecture" -o "build/Starward/app_$Version" -p:Platform=$Architecture -p:PublishReadyToRun=true -p:PublishTrimmed=true -p:TrimMode=partial -p:Version=$Version;
+    dotnet publish Starward -c Release -r "win10-$Architecture" -o "build/Starward/app-$Version" -p:Platform=$Architecture -p:PublishReadyToRun=true -p:PublishTrimmed=true -p:TrimMode=partial -p:Version=$Version;
 }
 
 $env:Path += ';C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\amd64\';
 
 msbuild Starward.Launcher "-property:Configuration=Release;Platform=$Architecture;OutDir=$(Resolve-Path "build/Starward/")";
 
-Add-Content "build/Starward/config.ini" -Value "app_folder=app_$Version`r`nexe_name=Starward.exe";
+Add-Content "build/Starward/config.ini" -Value "app_folder=app-$Version`r`nexe_name=Starward.exe";
 
 Remove-Item "build/Starward/Starward.pdb" -Force;
