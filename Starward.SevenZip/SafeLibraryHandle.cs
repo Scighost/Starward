856e3f8c6aaf2ab25cc09c1701b9a2cfcadfb8a0
Scighost <scighost@outlook.com>
2023-06-08 14:55:05 +0800

    add 7zip lib

---
+++ Starward.SevenZip/SafeLibraryHandle.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Win32.SafeHandles;
+using System.Runtime.ConstrainedExecution;
+
+namespace Starward.SevenZip
+{
+    internal sealed class SafeLibraryHandle : SafeHandleZeroOrMinusOneIsInvalid
+    {
+        public SafeLibraryHandle() : base(true)
+        {
+        }
+
+        /// <summary>Release library handle</summary>
+        /// <returns>true if the handle was released</returns>
+        [ReliabilityContract(Consistency.WillNotCorruptState, Cer.Success)]
+        protected override bool ReleaseHandle()
+        {
+            return Kernel32Dll.FreeLibrary(handle);
+        }
+    }
+}
---
+++ Starward.SevenZip/SevenZipException.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.Serialization;
+
+namespace Starward.SevenZip
+{
+    public class SevenZipException : Exception
+    {
+        public SevenZipException()
+        {
+        }
+
+        public SevenZipException(string message) : base(message)
+        {
+        }
+
+        public SevenZipException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected SevenZipException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
