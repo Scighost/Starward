d327a16cfc5624619daf15ef9f8b848564b78f85
Scighost <scighost@outlook.com>
2023-06-11 23:53:41 +0800

    game registry key

---
+++ Starward.Core/GameRegistry.cs
@@ -0,0 +1,50 @@
+﻿namespace Starward.Core;
+
+public class GameRegistry
+{
+
+
+    public const string InstallPath = "InstallPath";
+
+    public const string LauncherPath_hk4e_cn = @"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\原神";
+    public const string LauncherPath_hk4e_global = @"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Genshin Impact";
+    public const string LauncherPath_hk4e_cloud = @"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\云·原神";
+
+    public const string LauncherPath_hkrpg_cn = @"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\崩坏：星穹铁道";
+    public const string LauncherPath_hkrpg_global = @"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Star Rail";
+
+    public const string LauncherPath_bh3_cn = @"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\崩坏3";
+    public const string LauncherPath_bh3_overseas = @"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Honkai Impact 3";
+    public const string LauncherPath_bh3_global = @"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Honkai Impact 3rd";
+    public const string LauncherPath_bh3_tw = @"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\崩壊3rd";
+    public const string LauncherPath_bh3_kr = @"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\붕괴3rd";
+    public const string LauncherPath_bh3_jp = @"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\崩壊3rd";
+
+
+
+    public const string GamePath_hk4e_cn = @"HKEY_CURRENT_USER\Software\miHoYo\原神";
+    public const string GamePath_hk4e_global = @"HKEY_CURRENT_USER\Software\miHoYo\Genshin Impact";
+    public const string GamePath_hk4e_cloud = @"HKEY_CURRENT_USER\Software\miHoYo\云·原神";
+
+    public const string GamePath_hkrpg_cn = @"HKEY_CURRENT_USER\Software\miHoYo\崩坏：星穹铁道";
+    public const string GamePath_hkrpg_global = @"HKEY_CURRENT_USER\Software\Cognosphere\Star Rail";
+
+    public const string GamePath_bh3_cn = @"HKEY_CURRENT_USER\Software\miHoYo\崩坏3";
+    public const string GamePath_bh3_overseas = @"HKEY_CURRENT_USER\Software\miHoYo\Honkai Impact 3";
+    public const string GamePath_bh3_global = @"HKEY_CURRENT_USER\Software\miHoYo\Honkai Impact 3rd";
+    public const string GamePath_bh3_tw = @"HKEY_CURRENT_USER\Software\miHoYo\崩壊3rd";
+    public const string GamePath_bh3_kr = @"HKEY_CURRENT_USER\Software\miHoYo\붕괴3rd";
+    public const string GamePath_bh3_jp = @"HKEY_CURRENT_USER\Software\miHoYo\崩壊3rd";
+
+
+
+    public const string GENERAL_DATA_h2389025596 = "GENERAL_DATA_h2389025596";
+    public const string MIHOYOSDK_ADL_PROD_CN_h3123967166 = "MIHOYOSDK_ADL_PROD_CN_h3123967166";
+    public const string MIHOYOSDK_ADL_PROD_OVERSEA_h1158948810 = "MIHOYOSDK_ADL_PROD_OVERSEA_h1158948810";
+    public const string MIHOYOSDK_ADL_0 = "MIHOYOSDK_ADL_0";
+    public const string App_LastUserID_h2841727341 = "App_LastUserID_h2841727341";
+    public const string GraphicsSettings_Model_h2986158309 = "GraphicsSettings_Model_h2986158309";
+
+
+
+}
