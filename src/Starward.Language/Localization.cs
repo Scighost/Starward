6ecc466ee42ec3515fd1d8a4ae9183b70e90765c
Scighost <scighost@outlook.com>
2023-09-21 21:18:18 +0800

    add Russian locale selection

--- src/Starward.Language/Localization.cs
+++ src/Starward.Language/Localization.cs
@@ -9,11 +9,12 @@ public static class Localization
     {
         ("English (en-US, @Scighost)", "en-US"),
         ("日本語 (ja-JP, @reindex-ot)", "ja-JP"),
+        ("한국어 (ko-KR, @DE2SSE)", "ko-KR"),
+        ("Русский (ru-RU, @DarkAssassin48)", "ru-RU"),
+        ("ภาษาไทย (th-TH, @thunni-noi)", "th-TH"),
         ("Tiếng Việt (vi-VN, @phucho0237)", "vi-VN"),
         ("简体中文 (zh-CN, @Scighost)", "zh-CN"),
         ("繁體中文 (zh-TW, @XPRAMT)", "zh-TW"),
-        ("ภาษาไทย (th-TH, @thunni-noi)", "th-TH"),
-        ("한국어 (ko-KR, @DE2SSE)", "ko-KR"),
     }.AsReadOnly();
 
 
