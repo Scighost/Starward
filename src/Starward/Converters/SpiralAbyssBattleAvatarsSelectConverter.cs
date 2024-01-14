24b8265e02a032eef1ed5adabe9635a310c6fa44
Scighost <scighost@outlook.com>
2024-01-15 00:23:18 +0800

    fix the bug of spiral abyss page #554

---
+++ src/Starward/Converters/SpiralAbyssBattleAvatarsSelectConverter.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.UI.Xaml.Data;
+using Starward.Core.GameRecord.Genshin.SpiralAbyss;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Starward.Converters;
+
+public class SpiralAbyssBattleAvatarsSelectConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, string language)
+    {
+        if (value is SpiralAbyssLevel level)
+        {
+            if (parameter is "1")
+            {
+                if (level.Battles?.Count > 1)
+                {
+                    return level.Battles[1].Avatars;
+                }
+            }
+            else
+            {
+                return level.Battles?.FirstOrDefault()?.Avatars!;
+            }
+        }
+        if (value is IList<SpiralAbyssBattle> battles)
+        {
+            if (parameter is "1")
+            {
+                if (battles.Count > 1)
+                {
+                    return battles[1].Avatars;
+                }
+            }
+            else
+            {
+                return battles.FirstOrDefault()?.Avatars!;
+            }
+        }
+        return null!;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language)
+    {
+        throw new NotImplementedException();
+    }
+}
