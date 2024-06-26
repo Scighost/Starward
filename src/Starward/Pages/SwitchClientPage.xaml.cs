15197ef6ff915179147135238e84a4515932d701
Eric-Joker <1277243150@qq.com>
2024-06-27 01:40:54 +0800

    opt: Hide Powershell for switching client

--- src/Starward/Pages/SwitchClientPage.xaml.cs
+++ src/Starward/Pages/SwitchClientPage.xaml.cs
@@ -709,7 +709,7 @@ public sealed partial class SwitchClientPage : PageBase
                 FileName = "PowerShell",
                 Arguments = sb.ToString(),
                 UseShellExecute = true,
-                CreateNoWindow = true,
+                WindowStyle = ProcessWindowStyle.Hidden,
                 Verb = "runas",
             });
             if (p != null)
