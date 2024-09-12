efe518829704f80e95a81a036ba5ea5802956f27
hina <hina@hinaloe.net>
2024-09-13 00:05:30 +0900

    fix: revert Garbled
    thanks https://github.com/Scighost/Starward/pull/1038#discussion_r1756667700

--- src/Starward/Controls/BBSWebBridge.xaml.cs
+++ src/Starward/Controls/BBSWebBridge.xaml.cs
@@ -645,19 +645,19 @@ public sealed partial class BBSWebBridge : UserControl
     private class JsParam
     {
         /// <summary>
-        /// ��������
+        /// 方法名称
         /// </summary>
         [JsonPropertyName("method")]
         public string Method { get; set; }
 
         /// <summary>
-        /// ���� ����Ϊ��
+        /// 数据 可以为空
         /// </summary>
         [JsonPropertyName("payload")]
         public JsonNode? Payload { get; set; }
 
         /// <summary>
-        /// �ص������ƣ����� JavaScript:mhyWebBridge ʱ��Ϊ�׸���������
+        /// 回调的名称，调用 JavaScript:mhyWebBridge 时作为首个参数传入
         /// </summary>
         [JsonPropertyName("callback")]
         public string? Callback { get; set; }
@@ -668,19 +668,19 @@ public sealed partial class BBSWebBridge : UserControl
     private class JsResult
     {
         /// <summary>
-        /// ����
+        /// 代码
         /// </summary>
         [JsonPropertyName("retcode")]
         public int Code { get; set; }
 
         /// <summary>
-        /// ��Ϣ
+        /// 消息
         /// </summary>
         [JsonPropertyName("message")]
         public string Message { get; set; } = string.Empty;
 
         /// <summary>
-        /// ����
+        /// 数据
         /// </summary>
         [JsonPropertyName("data")]
         public Dictionary<string, object> Data { get; set; } = default!;
