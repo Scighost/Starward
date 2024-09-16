1e695eff804f9fa28e051ed03582e91da421e914
Scighost <scighost@outlook.com>
2024-09-16 20:47:46 +0800

    clean code

--- src/Starward.Core/EnumExtension.cs
+++ src/Starward.Core/EnumExtension.cs
@@ -1,5 +1,4 @@
 ﻿using Starward.Core.Launcher;
-using Starward.Core.SelfQuery;
 using System.ComponentModel;
 using System.Reflection;
 
@@ -35,53 +34,5 @@ public static class EnumExtension
             _ => "",
         };
     }
-
-
-
-    public static string ToLocalization(this GenshinQueryType genshinQueryType)
-    {
-        return genshinQueryType switch
-        {
-            GenshinQueryType.Crystal => CoreLang.GenshinQueryType_GenesisCrystal,
-            GenshinQueryType.Primogem => CoreLang.GenshinQueryType_Primogem,
-            GenshinQueryType.Resin => CoreLang.GenshinQueryType_OriginalResin,
-            GenshinQueryType.Artifact => CoreLang.GenshinQueryType_Artifact,
-            GenshinQueryType.Weapon => CoreLang.GenshinQueryType_Weapon,
-            _ => "",
-        };
-    }
-
-
-
-    public static string ToLocalization(this StarRailQueryType starRailQueryType)
-    {
-        return starRailQueryType switch
-        {
-            StarRailQueryType.Stellar => CoreLang.StarRailQueryType_StellarJade,
-            StarRailQueryType.Dreams => CoreLang.StarRailQueryType_OneiricShared,
-            StarRailQueryType.Relic => CoreLang.StarRailQueryType_Relic,
-            StarRailQueryType.Cone => CoreLang.StarRailQueryType_LightCone,
-            StarRailQueryType.Power => CoreLang.StarRailQueryType_TrailblazePower,
-            _ => "",
-        };
-    }
-
-
-
-    public static string ToLocalization(this ZZZQueryType zZZQueryType)
-    {
-        return zZZQueryType switch
-        {
-            ZZZQueryType.Monochrome => CoreLang.ZZZQueryType_Monochrome,
-            ZZZQueryType.Ploychrome => CoreLang.ZZZQueryType_Ploychrome,
-            ZZZQueryType.PurchaseGift => CoreLang.ZZZQueryType_Bundle,
-            ZZZQueryType.Battery => CoreLang.ZZZQueryType_BatteryCharge,
-            ZZZQueryType.Engine => CoreLang.ZZZQueryType_WEngine,
-            ZZZQueryType.Disk => CoreLang.ZZZQueryType_DriveDisc,
-            _ => "",
-        };
-    }
-
-
 
 }
---
+++ src/Starward.Core/SelfQuery/SelfQueryTpyeEnumExtension.cs
@@ -0,0 +1,50 @@
+﻿namespace Starward.Core.SelfQuery;
+
+public static class SelfQueryTpyeEnumExtension
+{
+
+    public static string ToLocalization(this GenshinQueryType genshinQueryType)
+    {
+        return genshinQueryType switch
+        {
+            GenshinQueryType.Crystal => CoreLang.GenshinQueryType_GenesisCrystal,
+            GenshinQueryType.Primogem => CoreLang.GenshinQueryType_Primogem,
+            GenshinQueryType.Resin => CoreLang.GenshinQueryType_OriginalResin,
+            GenshinQueryType.Artifact => CoreLang.GenshinQueryType_Artifact,
+            GenshinQueryType.Weapon => CoreLang.GenshinQueryType_Weapon,
+            _ => "",
+        };
+    }
+
+
+
+    public static string ToLocalization(this StarRailQueryType starRailQueryType)
+    {
+        return starRailQueryType switch
+        {
+            StarRailQueryType.Stellar => CoreLang.StarRailQueryType_StellarJade,
+            StarRailQueryType.Dreams => CoreLang.StarRailQueryType_OneiricShared,
+            StarRailQueryType.Relic => CoreLang.StarRailQueryType_Relic,
+            StarRailQueryType.Cone => CoreLang.StarRailQueryType_LightCone,
+            StarRailQueryType.Power => CoreLang.StarRailQueryType_TrailblazePower,
+            _ => "",
+        };
+    }
+
+
+
+    public static string ToLocalization(this ZZZQueryType zZZQueryType)
+    {
+        return zZZQueryType switch
+        {
+            ZZZQueryType.Monochrome => CoreLang.ZZZQueryType_Monochrome,
+            ZZZQueryType.Ploychrome => CoreLang.ZZZQueryType_Ploychrome,
+            ZZZQueryType.PurchaseGift => CoreLang.ZZZQueryType_Bundle,
+            ZZZQueryType.Battery => CoreLang.ZZZQueryType_BatteryCharge,
+            ZZZQueryType.Engine => CoreLang.ZZZQueryType_WEngine,
+            ZZZQueryType.Disk => CoreLang.ZZZQueryType_DriveDisc,
+            _ => "",
+        };
+    }
+
+}
